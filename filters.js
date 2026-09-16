3b0fbe64b5b4850ff06a97e48e669ae6bed93f1f
Adhikrz <adhiikrz@gmail.com>
2026-09-16 22:46:40 +0530

    Update filters.js

--- filters.js
+++ filters.js
@@ -1,552 +1,120 @@
-window.DEFAULT_FILTERS = [
-  {
-    "id": "f1",
-    "name": "X-Ray Scan",
-    "category": "Original 110"
-  },
-  {
-    "id": "f2",
-    "name": "Banknote Engraving",
-    "category": "Original 110"
-  },
-  {
-    "id": "f3",
-    "name": "Wire Photo",
-    "category": "Original 110"
-  },
-  {
-    "id": "f4",
-    "name": "Giallo Gels",
-    "category": "Original 110"
-  },
-  {
-    "id": "f5",
-    "name": "Two-Strip",
-    "category": "Original 110"
-  },
-  {
-    "id": "f6",
-    "name": "Aerochrome",
-    "category": "Original 110"
-  },
-  {
-    "id": "f7",
-    "name": "Slow Shutter",
-    "category": "Original 110"
-  },
-  {
-    "id": "f8",
-    "name": "Full Stitch",
-    "category": "Original 110"
-  },
-  {
-    "id": "f9",
-    "name": "Aura Gradient",
-    "category": "Original 110"
-  },
-  {
-    "id": "f10",
-    "name": "Night Vision",
-    "category": "Original 110"
-  },
-  {
-    "id": "f11",
-    "name": "Blue Negative",
-    "category": "Original 110"
-  },
-  {
-    "id": "f12",
-    "name": "LED Matrix",
-    "category": "Original 110"
-  },
-  {
-    "id": "f13",
-    "name": "Chemical Frost",
-    "category": "Original 110"
-  },
-  {
-    "id": "f14",
-    "name": "Ink Bloom",
-    "category": "Original 110"
-  },
-  {
-    "id": "f15",
-    "name": "Photogram",
-    "category": "Original 110"
-  },
-  {
-    "id": "f16",
-    "name": "Heat Trace",
-    "category": "Original 110"
-  },
-  {
-    "id": "f17",
-    "name": "Scan Lines",
-    "category": "Original 110"
-  },
-  {
-    "id": "f18",
-    "name": "Film Soup",
-    "category": "Original 110"
-  },
-  {
-    "id": "f19",
-    "name": "Cyanotype",
-    "category": "Original 110"
-  },
-  {
-    "id": "f20",
-    "name": "Fogged Glass",
-    "category": "Original 110"
-  },
-  {
-    "id": "f21",
-    "name": "Dead Pixels",
-    "category": "Original 110"
-  },
-  {
-    "id": "f22",
-    "name": "Low Poly",
-    "category": "Original 110"
-  },
-  {
-    "id": "f23",
-    "name": "256 Colors",
-    "category": "Original 110"
-  },
-  {
-    "id": "f24",
-    "name": "Blue Flood",
-    "category": "Original 110"
-  },
-  {
-    "id": "f25",
-    "name": "Herbarium",
-    "category": "Original 110"
-  },
-  {
-    "id": "f26",
-    "name": "Density Map",
-    "category": "Original 110"
-  },
-  {
-    "id": "f27",
-    "name": "Thermal Print",
-    "category": "Original 110"
-  },
-  {
-    "id": "f28",
-    "name": "Paint By Number",
-    "category": "Original 110"
-  },
-  {
-    "id": "f29",
-    "name": "Encyclopedia Plate",
-    "category": "Original 110"
-  },
-  {
-    "id": "f30",
-    "name": "Ransom Collage",
-    "category": "Original 110"
-  },
-  {
-    "id": "f31",
-    "name": "Wet Emulsion",
-    "category": "Original 110"
-  },
-  {
-    "id": "f32",
-    "name": "Desktop 98",
-    "category": "Original 110"
-  },
-  {
-    "id": "f33",
-    "name": "Cursor Swarm",
-    "category": "Original 110"
-  },
-  {
-    "id": "f34",
-    "name": "Cutout Redaction",
-    "category": "Original 110"
-  },
-  {
-    "id": "f35",
-    "name": "Scribble Riot",
-    "category": "Original 110"
-  },
-  {
-    "id": "f36",
-    "name": "Paper Crumple",
-    "category": "Original 110"
-  },
-  {
-    "id": "f37",
-    "name": "Solarized Print",
-    "category": "Original 110"
-  },
-  {
-    "id": "f38",
-    "name": "Marker Makeup",
-    "category": "Original 110"
-  },
-  {
-    "id": "f39",
-    "name": "Deadpan Toon",
-    "category": "Original 110"
-  },
-  {
-    "id": "f40",
-    "name": "Chirashi Flyer",
-    "category": "Original 110"
-  },
-  {
-    "id": "f41",
-    "name": "Street Poster",
-    "category": "Original 110"
-  },
-  {
-    "id": "f42",
-    "name": "Cross Stitch",
-    "category": "Original 110"
-  },
-  {
-    "id": "f43",
-    "name": "Blind Emboss",
-    "category": "Original 110"
-  },
-  {
-    "id": "f44",
-    "name": "Overprint",
-    "category": "Original 110"
-  },
-  {
-    "id": "f45",
-    "name": "Sticker Bomb",
-    "category": "Original 110"
-  },
-  {
-    "id": "f46",
-    "name": "Bubble Wrapped",
-    "category": "Original 110"
-  },
-  {
-    "id": "f47",
-    "name": "Liquid Chrome",
-    "category": "Original 110"
-  },
-  {
-    "id": "f48",
-    "name": "Specimen Sheet",
-    "category": "Original 110"
-  },
-  {
-    "id": "f49",
-    "name": "Misprint Echo",
-    "category": "Original 110"
-  },
-  {
-    "id": "f50",
-    "name": "Camcorder HUD",
-    "category": "Original 110"
-  },
-  {
-    "id": "f51",
-    "name": "VHS Chrome",
-    "category": "Original 110"
-  },
-  {
-    "id": "f52",
-    "name": "Split Halftone",
-    "category": "Original 110"
-  },
-  {
-    "id": "f53",
-    "name": "Indexed Dither",
-    "category": "Original 110"
-  },
-  {
-    "id": "f54",
-    "name": "Stencil Fluoro",
-    "category": "Original 110"
-  },
-  {
-    "id": "f55",
-    "name": "Sabattier Push",
-    "category": "Original 110"
-  },
-  {
-    "id": "f56",
-    "name": "Voxel Relief",
-    "category": "Original 110"
-  },
-  {
-    "id": "f57",
-    "name": "Amber Glamour",
-    "category": "Original 110"
-  },
-  {
-    "id": "f58",
-    "name": "Screen Engraving",
-    "category": "Original 110"
-  },
-  {
-    "id": "f59",
-    "name": "Thermal Liquid",
-    "category": "Original 110"
-  },
-  {
-    "id": "f60",
-    "name": "Fog Silhouette",
-    "category": "Original 110"
-  },
-  {
-    "id": "f61",
-    "name": "Pink Bloom",
-    "category": "Original 110"
-  },
-  {
-    "id": "f62",
-    "name": "Rescreen",
-    "category": "Original 110"
-  },
-  {
-    "id": "f63",
-    "name": "Teletext",
-    "category": "Original 110"
-  },
-  {
-    "id": "f64",
-    "name": "Redscale Flash",
-    "category": "Original 110"
-  },
-  {
-    "id": "f65",
-    "name": "Heat Negative",
-    "category": "Original 110"
-  },
-  {
-    "id": "f66",
-    "name": "Chromatography",
-    "category": "Original 110"
-  },
-  {
-    "id": "f67",
-    "name": "Vector Dissect",
-    "category": "Original 110"
-  },
-  {
-    "id": "f68",
-    "name": "Dragged Shutter",
-    "category": "Original 110"
-  },
-  {
-    "id": "f69",
-    "name": "Screen Clash",
-    "category": "Original 110"
-  },
-  {
-    "id": "f70",
-    "name": "Chemigram",
-    "category": "Original 110"
-  },
-  {
-    "id": "f71",
-    "name": "Expired Tungsten",
-    "category": "Original 110"
-  },
-  {
-    "id": "f72",
-    "name": "Pastel Thermal",
-    "category": "Original 110"
-  },
-  {
-    "id": "f73",
-    "name": "Paste-Up Collage",
-    "category": "Original 110"
-  },
-  {
-    "id": "f74",
-    "name": "Overexposure Bloom",
-    "category": "Original 110"
-  },
-  {
-    "id": "f75",
-    "name": "Slit Scan",
-    "category": "Original 110"
-  },
-  {
-    "id": "f76",
-    "name": "Platen Drag",
-    "category": "Original 110"
-  },
-  {
-    "id": "f77",
-    "name": "Blacklight",
-    "category": "Original 110"
-  },
-  {
-    "id": "f78",
-    "name": "Forensic Photo",
-    "category": "Original 110"
-  },
-  {
-    "id": "f79",
-    "name": "Lenticular",
-    "category": "Original 110"
-  },
-  {
-    "id": "f80",
-    "name": "Pinscreen",
-    "category": "Original 110"
-  },
-  {
-    "id": "f81",
-    "name": "Electron Scan",
-    "category": "Original 110"
-  },
-  {
-    "id": "f82",
-    "name": "Daguerreotype",
-    "category": "Original 110"
-  },
-  {
-    "id": "f83",
-    "name": "Autochrome",
-    "category": "Original 110"
-  },
-  {
-    "id": "f84",
-    "name": "Jacquard Weave",
-    "category": "Original 110"
-  },
-  {
-    "id": "f85",
-    "name": "4-Shade LCD",
-    "category": "Original 110"
-  },
-  {
-    "id": "f86",
-    "name": "Dye Halo",
-    "category": "Original 110"
-  },
-  {
-    "id": "f87",
-    "name": "Light Painted",
-    "category": "Original 110"
-  },
-  {
-    "id": "f88",
-    "name": "Edge Trace",
-    "category": "Original 110"
-  },
-  {
-    "id": "f89",
-    "name": "VCR OSD",
-    "category": "Original 110"
-  },
-  {
-    "id": "f90",
-    "name": "Surveillance Dossier",
-    "category": "Original 110"
-  },
-  {
-    "id": "f91",
-    "name": "Thermal Mask",
-    "category": "Original 110"
-  },
-  {
-    "id": "f92",
-    "name": "Projector Burn",
-    "category": "Original 110"
-  },
-  {
-    "id": "f93",
-    "name": "Riso Bloom",
-    "category": "Original 110"
-  },
-  {
-    "id": "f94",
-    "name": "Radiograph Zine",
-    "category": "Original 110"
-  },
-  {
-    "id": "f95",
-    "name": "Pixel Sort",
-    "category": "Original 110"
-  },
-  {
-    "id": "f96",
-    "name": "Archive Rot",
-    "category": "Original 110"
-  },
-  {
-    "id": "f97",
-    "name": "Three Inks",
-    "category": "Original 110"
-  },
-  {
-    "id": "f98",
-    "name": "Scrap Stitched",
-    "category": "Original 110"
-  },
-  {
-    "id": "f99",
-    "name": "Bleach Dye",
-    "category": "Original 110"
-  },
-  {
-    "id": "f100",
-    "name": "Stitch Chart",
-    "category": "Original 110"
-  },
-  {
-    "id": "f101",
-    "name": "Dot Interference",
-    "category": "Original 110"
-  },
-  {
-    "id": "f102",
-    "name": "Cyber Sigil",
-    "category": "Original 110"
-  },
-  {
-    "id": "f103",
-    "name": "Chrome Rorschach",
-    "category": "Original 110"
-  },
-  {
-    "id": "f104",
-    "name": "Heat Contour",
-    "category": "Original 110"
-  },
-  {
-    "id": "f105",
-    "name": "Pixel Lace",
-    "category": "Original 110"
-  },
-  {
-    "id": "f106",
-    "name": "Pink Phosphor",
-    "category": "Original 110"
-  },
-  {
-    "id": "f107",
-    "name": "Dropped Stitch",
-    "category": "Original 110"
-  },
-  {
-    "id": "f108",
-    "name": "Hyper Slice",
-    "category": "Original 110"
-  },
-  {
-    "id": "f109",
-    "name": "Bio HUD",
-    "category": "Original 110"
-  },
-  {
-    "id": "f110",
-    "name": "Poison Copy",
-    "category": "Original 110"
-  }
+window.FILTERS = [
+
+  "X-Ray Scan",
+  "Banknote Engraving",
+  "Wire Photo",
+  "Giallo Gels",
+  "Two-Strip",
+  "Aerochrome",
+  "Slow Shutter",
+  "Full Stitch",
+  "Aura Gradient",
+  "Night Vision",
+  "Blue Negative",
+  "LED Matrix",
+  "Chemical Frost",
+  "Ink Bloom",
+  "Photogram",
+  "Heat Trace",
+  "Scan Lines",
+  "Film Soup",
+  "Cyanotype",
+  "Fogged Glass",
+  "Dead Pixels",
+  "Low Poly",
+  "256 Colors",
+  "Blue Flood",
+  "Herbarium",
+  "Density Map",
+  "Thermal Print",
+  "Paint By Number",
+  "Encyclopedia Plate",
+  "Ransom Collage",
+  "Wet Emulsion",
+  "Desktop 98",
+  "Cursor Swarm",
+  "Cutout Redaction",
+  "Scribble Riot",
+  "Paper Crumple",
+  "Solarized Print",
+  "Marker Makeup",
+  "Deadpan Toon",
+  "Chirashi Flyer",
+  "Street Poster",
+  "Cross Stitch",
+  "Blind Emboss",
+  "Overprint",
+  "Sticker Bomb",
+  "Bubble Wrapped",
+  "Liquid Chrome",
+  "Specimen Sheet",
+  "Misprint Echo",
+  "Camcorder HUD",
+  "VHS Chrome",
+  "Split Halftone",
+  "Indexed Dither",
+  "Stencil Fluoro",
+  "Sabattier Push",
+  "Voxel Relief",
+  "Amber Glamour",
+  "Screen Engraving",
+  "Thermal Liquid",
+  "Fog Silhouette",
+  "Pink Bloom",
+  "Rescreen",
+  "Teletext",
+  "Redscale Flash",
+  "Heat Negative",
+  "Chromatography",
+  "Vector Dissect",
+  "Dragged Shutter",
+  "Screen Clash",
+  "Chemigram",
+  "Expired Tungsten",
+  "Pastel Thermal",
+  "Paste-Up Collage",
+  "Overexposure Bloom",
+  "Slit Scan",
+  "Platen Drag",
+  "Blacklight",
+  "Forensic Photo",
+  "Lenticular",
+  "Pinscreen",
+  "Electron Scan",
+  "Daguerreotype",
+  "Autochrome",
+  "Jacquard Weave",
+  "4-Shade LCD",
+  "Dye Halo",
+  "Light Painted",
+  "Edge Trace",
+  "VCR OSD",
+  "Surveillance Dossier",
+  "Thermal Mask",
+  "Projector Burn",
+  "Riso Bloom",
+  "Radiograph Zine",
+  "Pixel Sort",
+  "Archive Rot",
+  "Three Inks",
+  "Scrap Stitched",
+  "Bleach Dye",
+  "Stitch Chart",
+  "Dot Interference",
+  "Cyber Sigil",
+  "Chrome Rorschach",
+  "Heat Contour",
+  "Pixel Lace",
+  "Pink Phosphor",
+  "Dropped Stitch",
+  "Hyper Slice",
+  "Bio HUD",
+  "Poison Copy"
+
 ];
+
+window.FILTERS = window.FILTERS.map((name, index) => ({
+  id: index + 1,
+  name: name,
+  desc: "Experimental image transformation"
+}));
